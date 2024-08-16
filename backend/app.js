8bb691853363d4cd349190407d8182e70a916572
ribhavsingla <ribhavsingla2166@gmail.com>
2024-08-16 18:24:07 +0530

    fixing_cors_origin

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const User = require('./models/user.js');
 const app = express();
 const cors = require('cors');
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Specify your origin
+  res.header('Access-Control-Allow-Origin', 'https://foodio-com-project-wflq.onrender.com'); // Specify your origin
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow specific HTTP methods
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
   res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials
