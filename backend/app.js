28a6d0ab7005ed782795f8f3f8abdb99ca39fe35
ribhavsingla <ribhavsingla2166@gmail.com>
2024-08-16 14:34:31 +0530

    added_middleware_cors

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,19 @@ const sessionOptions = {
     httpOnly : true,
   }
 }
+
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:5173', 'https://foodio-com-project.onrender.com', 'http://foodio-com-project.onrender.com'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
   
 // middlewares
 app.use(express.urlencoded({ extended: true }));
